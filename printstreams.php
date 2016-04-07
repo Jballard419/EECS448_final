8bade1213f83fd2c6d71e9c61edcff79d91c325f
Brandon Givens <brandon.a.givens@gmail.com>
2016-04-07 10:18:15 -0500

    Add CSS syling Moved portions of content to own panels

--- printstreams.php
+++ printstreams.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 $dataArray = json_decode(@file_get_contents('https://api.twitch.tv/kraken/games/top'), true);
-
+echo "<div id=\"leftpanel\"";
 echo "<center>"."<font face=\"arial\" size=\"6\"><b>"."Current Top Games on Twitch"."</b></font>"."<br>";
 
 echo "<font face=\"arial\" size =\"4\">";
@@ -12,5 +12,5 @@ echo "<font face=\"arial\" size =\"4\">";
 for ($i=0; $i <10 ; $i++) {
   echo "<br>" . $dataArray['top'][$i]['game']['name'] ."</br>";
 }
-echo "</font>"."</center>";
+echo "</font>"."</center>"."</div>";
  ?>
