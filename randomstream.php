5fd2b1c79e9a2f32276ceb10ecc3b29876e029cc
Brandon Givens <brandon.a.givens@gmail.com>
2016-05-10 19:20:31 -0500

    CSS the panels

--- randomstream.php
+++ randomstream.php
@@ -10,24 +10,23 @@ if(isset($_GET["channel_name"]))
 {
   $cname = $_GET["channel_name"];
 
-  echo "<div align='left'>
+  echo "
+	<div id='rightpanel'>
+	<div align='left'>
+	
+      	<object type='application/x-shockwave-flash' height='500' width='900'     id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel=".$cname."' bgcolor='#000000'>
+          	<param name='allowFullScreen' value='true' />
+          	<param name='allowScriptAccess' value='always' />
+          	<param name='allowNetworking' value='all' />
+          	<param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' />
+          	<param name='flashvars' value='hostname=www.twitch.tv&channel=".$cname."&auto_play=true&start_volume=25' />
+       </object></div>
+       
+  	<div>
+     		<iframe frameborder='0' scrolling='no' src='http://twitch.tv/".$cname."/chat?popout=' height='500' width='900'>
+       		</iframe>
+        </div>
+</div>";
 
-      <object type='application/x-shockwave-flash' height='500' width='900'     id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel=".$cname."' bgcolor='#000000'>
-          <param name='allowFullScreen' value='true' />
-          <param name='allowScriptAccess' value='always' />
-          <param name='allowNetworking' value='all' />
-          <param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' />
-          <param name='flashvars' value='hostname=www.twitch.tv&channel=".$cname."&auto_play=true&start_volume=25' />
-       </object>
-       </object>
-
-
-
-  <div align='right'>
-     <iframe frameborder='0' scrolling='no' src='http://twitch.tv/".$cname."/chat?popout=' height='500' width='350'>
-       </iframe>
-       </iframe>
-
-  </div>";
 }
 ?>
